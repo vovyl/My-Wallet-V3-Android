ae8597680b3aa935d15b024eb8d34255707656bd
Adam Bennett <adam@blockchain.com>
2018-03-13 16:43:31 +0000

    refactor(JAR): Attempt to fix test coverage

--- scripts/ci_unit_tests.sh
+++ scripts/ci_unit_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./gradlew testEnvProdMinApi21DebugUnitTestCoverage coveralls -Dpre-dex=false -Pkotlin.incremental=false --stacktrace
+./gradlew generateTestCoverage coveralls -Dpre-dex=false -Pkotlin.incremental=false --stacktrace
