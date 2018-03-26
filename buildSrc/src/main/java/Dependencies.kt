7068f5e3bb3dca102038f41b5bd97dfd21abdfb9
Adam Bennett <adam@blockchain.com>
2018-03-26 14:42:52 +0100

    Increment version code: 6.11.0-rc1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val minSdk = 16
     const val targetSdk = 27
     const val compileSdk = 27
-    const val versionCode = 300
+    const val versionCode = 301
     const val versionName = "6.11.0-rc1"
     const val buildTools = "27.0.3"
 
