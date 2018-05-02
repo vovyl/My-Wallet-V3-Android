1391e9385772d712d939e74cc8f0798c163825f0
Riaan Vos <riaan.vos@blockchain.com>
2018-05-02 13:33:24 +0100

    feat(Buy Sell): Add custom chrome tab for iSignThis. Remove unneeded fragment.

--- app/src/main/java/piuk/blockchain/android/ui/buysell/coinify/signup/verify_identification/CoinifyVerifyIdentificationView.kt
+++
@@ -1,8 +0,0 @@
-package piuk.blockchain.android.ui.buysell.coinify.signup.verify_identification
-
-import piuk.blockchain.androidcoreui.ui.base.View
-
-interface CoinifyVerifyIdentificationView: View {
-
-    fun onStartOverview()
-}
