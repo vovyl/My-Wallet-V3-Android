743c32959bd4f624e4432eb04b67fe61539ffa41
Adam Bennett <adam@blockchain.com>
2018-06-28 10:22:55 +0100

    chore(Dependencies): Update Crashlytics to latest version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -66,7 +66,7 @@ object Versions {
     // Logging
     const val timber = "4.6.0"
     const val slf4j = "1.7.20"
-    const val crashlytics = "2.9.1"
+    const val crashlytics = "2.9.4"
     const val fabricTools = "1.24.4"
 
     // Testing
