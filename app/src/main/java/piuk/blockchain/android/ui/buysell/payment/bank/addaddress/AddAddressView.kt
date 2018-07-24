18ba1a44b60ac477e6dc1ee0270cb7087708d003
Adam Bennett <ditn@users.noreply.github.com>
2018-07-24 13:20:43 +0100

    fix(Sell): AND-1292 Set currency code of bank account from quote, not from user's default currency (#1048)

--- app/src/main/java/piuk/blockchain/android/ui/buysell/payment/bank/addaddress/AddAddressView.kt
+++ app/src/main/java/piuk/blockchain/android/ui/buysell/payment/bank/addaddress/AddAddressView.kt
@@ -1,6 +1,7 @@
 package piuk.blockchain.android.ui.buysell.payment.bank.addaddress
 
 import android.support.annotation.StringRes
+import piuk.blockchain.android.ui.buysell.createorder.models.SellConfirmationDisplayModel
 import piuk.blockchain.androidcoreui.ui.base.View
 import piuk.blockchain.androidcoreui.ui.customviews.ToastCustom
 
@@ -9,6 +10,8 @@ interface AddAddressView : View {
     val iban: String
 
     val bic: String
+
+    val displayModel: SellConfirmationDisplayModel
 
     val accountHolderName: String
 
