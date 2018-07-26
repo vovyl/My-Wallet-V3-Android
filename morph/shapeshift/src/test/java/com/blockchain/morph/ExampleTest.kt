efab8064accd2585fb4b9ffc68d67a7fab420661
Alan Evans <alan@blockchain.com>
2018-07-26 14:32:29 -0300

    refactor(ShapeShift): AND-1249 New modules for Morph abstraction of Shapeshift (#1059)

---
+++ morph/shapeshift/src/test/java/com/blockchain/morph/ExampleTest.kt
@@ -0,0 +1,12 @@
+package com.blockchain.morph
+
+import org.amshove.kluent.`should be`
+import org.junit.Test
+
+class ExampleTest {
+
+    @Test
+    fun `I pass`() {
+        1 `should be` 1
+    }
+}
