2bae29647a9cd5bf52a47791065101bd41dec23b
Alan Evans <alan@blockchain.com>
2018-10-02 14:46:01 -0300

    feat(Sunriver): AND-1497 New module (#3)

---
+++ sunriver/src/test/kotlin/com/blockchain/sunriver/ExampleTest.kt
@@ -0,0 +1,12 @@
+package com.blockchain.sunriver
+
+import org.amshove.kluent.`should be`
+import org.junit.Test
+
+class ExampleTest {
+
+    @Test
+    fun `Example Test`() {
+        1 + 2 `should be` 3
+    }
+}
