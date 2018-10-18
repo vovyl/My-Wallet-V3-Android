381cd9c117b8c3daff116714d9248956c6b4de5f
Adam Bennett <ditn@users.noreply.github.com>
2018-10-18 10:44:57 +0100

    feat(Homebrew): AND-1299 Remove build flag, update version code (#62)

--- app/src/main/java/piuk/blockchain/android/injection/KycModule.kt
+++ app/src/main/java/piuk/blockchain/android/injection/KycModule.kt
@@ -1,23 +1,15 @@
 package piuk.blockchain.android.injection
 
-import com.blockchain.kyc.DisabledFeatureFlag
 import com.blockchain.remoteconfig.FeatureFlag
 import dagger.Module
 import dagger.Provides
 import info.blockchain.wallet.api.WalletApi
-import piuk.blockchain.android.BuildConfig
 import piuk.blockchain.android.kyc.KycServerSideFeatureFlag
-import timber.log.Timber
 
 @Module
 class KycModule {
 
     @Provides
     fun provideFeatureFlag(walletApi: dagger.Lazy<WalletApi>): FeatureFlag =
-        if (BuildConfig.HOMEBREW_DISABLED) {
-            Timber.w("Homebrew is disabled in build")
-            DisabledFeatureFlag()
-        } else {
-            KycServerSideFeatureFlag(walletApi.get())
-        }
+        KycServerSideFeatureFlag(walletApi.get())
 }
