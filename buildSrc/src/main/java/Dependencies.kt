d90915bd094adf853c6b88af83035d0e009cb8ac
Adam Bennett <ditn@users.noreply.github.com>
2018-10-29 16:15:26 +0000

    chore(Release): AND-1643 Increment version codes for release (#118)

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,8 +6,8 @@ object Versions {
     const val minSdk = 17
     const val targetSdk = 27
     const val compileSdk = 27
-    const val versionCode = 334
-    const val versionName = "6.14.2"
+    const val versionCode = 335
+    const val versionName = "6.14.3"
     const val buildTools = "28.0.3"
 
     // Build tools and languages
