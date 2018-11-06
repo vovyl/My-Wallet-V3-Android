ac88f825cc2d3f564f7312a93be8bb6b3161bcc3
Adam Bennett <ditn@users.noreply.github.com>
2018-11-06 19:22:57 +0000

    fix(Send): AND-1656 Fix issue which prevented BCH and BTC sends from completing (#179)

--- coreui/src/main/java/piuk/blockchain/androidcoreui/utils/logging/CustomEventBuilder.kt
+++ coreui/src/main/java/piuk/blockchain/androidcoreui/utils/logging/CustomEventBuilder.kt
@@ -2,7 +2,7 @@ package piuk.blockchain.androidcoreui.utils.logging
 
 abstract class CustomEventBuilder(internal val eventName: String) {
 
-    private val customEvents = mutableMapOf<String, String>()
+    private val customEvents: MutableMap<String, String> = mutableMapOf()
 
     protected fun putCustomAttribute(key: String, value: String) {
         customEvents[key] = value
@@ -13,6 +13,6 @@ abstract class CustomEventBuilder(internal val eventName: String) {
     }
 
     internal fun build(action: (key: String, value: String) -> Unit) {
-        customEvents.forEach(action)
+        customEvents.forEach { action(it.key, it.value) }
     }
 }
