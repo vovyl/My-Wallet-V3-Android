dc4322ff7df4f16b17415440eb7f3642abcc1bd7
Alan Evans <alan@blockchain.com>
2019-01-21 09:09:36 -0400

    feat(Veriff): AND-1840 Veriff 1.6.8 - manual update (#284)

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,8 +6,8 @@ object Versions {
     const val minSdk = 19
     const val targetSdk = 27
     const val compileSdk = 27
-    const val versionCode = 405
-    const val versionName = "6.17.0"
+    const val versionCode = 406
+    const val versionName = "6.17.1-beta1"
     const val buildTools = "28.0.3"
 
     // Build tools and languages
