dd0bafa2afffcf092a06493c1929cd653c3d3c0a
Alan Evans <alan@blockchain.com>
2019-01-29 14:17:13 -0400

    feat(Veriff): AND-1875 Resubmission Json format (#291)

--- kyc/src/main/java/com/blockchain/kyc/models/nabu/NabuUser.kt
+++ kyc/src/main/java/com/blockchain/kyc/models/nabu/NabuUser.kt
@@ -18,7 +18,7 @@ data class NabuUser(
     val address: Address?,
     val state: UserState,
     val kycState: KycState,
-    val resubmission: String? = null,
+    val resubmission: Any? = null,
     /**
      * ISO-8601 Timestamp w/millis, eg 2018-08-15T17:00:45.129Z
      */
