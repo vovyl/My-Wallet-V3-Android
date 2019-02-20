9f6e104e8f385e32ac6746b30a25a5d58f042985
Alan Evans <alan@blockchain.com>
2019-02-20 12:56:32 -0400

    chore(Release): AND-1929 Update version code

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val minSdk = 19
     const val targetSdk = 27
     const val compileSdk = 27
-    const val versionCode = 425
+    const val versionCode = 426
     const val versionName = "6.19.0"
     const val buildTools = "28.0.3"
 
